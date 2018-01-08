136fe172c34f04c86a54c80c673920cbd4f2516a
StarNeit <koneit@outlook.com>
2018-01-08 15:01:08 +0100

    update

--- scripts/donut-chart.js
+++ scripts/donut-chart.js
@@ -27,33 +27,4 @@ var config = {
         cutoutPercentage: 70,
     }
 };
-
-Chart.pluginService.register({
-    beforeDraw: function(chart) {
-        var width = chart.chart.width,
-            height = chart.chart.height,
-            ctx = chart.chart.ctx;
-
-        ctx.restore();
-        ctx.font = "24px ProximaNovaRegular";
-        ctx.textBaseline = "middle";
-
-        var text = "211,298",
-            textX = Math.round((width - ctx.measureText(text).width) / 2),
-            textY = height / 2;
-        ctx.fillText(text, textX, textY);
-        ctx.save();
-
-
-        ctx.restore();
-        ctx.font = "10px ProximaNovaRegular";
-        ctx.textBaseline = "middle";
-
-        var text2 = "Websites",
-            textX2 = Math.round((width - ctx.measureText(text2).width) / 2),
-            textY2 = height / 2 + 20;
-        ctx.fillText(text2, textX2, textY2);
-        ctx.save();
-    }
-});
 new Chart(donutEl, config);
